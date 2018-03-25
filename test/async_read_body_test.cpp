0ebef44a2d819ebace36a20d67e72e5c78cb3eec
LeonineKing1199 <christian.mazakas@gmail.com>
2018-03-25 14:57:47 -0700

    Useless stackful coros, updated stream

--- test/async_read_body_test.cpp
+++ test/async_read_body_test.cpp
@@ -54,22 +54,26 @@ TEST_CASE("async_read_body")
 
     foxy::header_parser<> header_parser;
 
-    auto const bytes_read = http::read_header(stream, buf, header_parser);
-    REQUIRE(bytes_read > 0);
-    REQUIRE(header_parser.is_header_done());
-
-    auto const target = header_parser.get().target();
-    REQUIRE(target == "/rawr");
-
     asio::spawn(
+      ioc,
       [&](asio::yield_context yield_context) -> void
       {
+        auto const bytes_read =
+          http::async_read_header(stream, buf, header_parser, yield_context);
+
+        REQUIRE(bytes_read > 0);
+        REQUIRE(header_parser.is_header_done());
+
+        auto const target = header_parser.get().target();
+        REQUIRE(target == "/rawr");
+
         auto msg = foxy::async_read_body<http::string_body>(
           stream, buf, timer,
           std::move(header_parser),
           yield_context);
 
         REQUIRE(msg.body() == "I bestow the heads of virgins and the first-born sons.");
+        ioc.stop();
       });
 
     ioc.run();
@@ -99,58 +103,67 @@ TEST_CASE("async_read_body")
     foxy::header_parser<> header_parser;
     header_parser.body_limit(body_size);
 
-    auto const bytes_read = http::read_header(stream, buf, header_parser);
-    REQUIRE(bytes_read > 0);
-    REQUIRE(header_parser.is_header_done());
-
     asio::spawn(
+      io,
       [&](asio::yield_context yield_context) -> void
       {
+        auto const bytes_read =
+          http::async_read_header(stream, buf, header_parser, yield_context);
+
+        REQUIRE(bytes_read > 0);
+        REQUIRE(header_parser.is_header_done());
+
         auto req = foxy::async_read_body<body_t>(
           stream, buf, timer,
           std::move(header_parser),
           yield_context);
 
         REQUIRE(req.body().size() == body_size);
+        io.stop();
+      });
+
+    REQUIRE(io.run() > 0);
+  }
+
+  SECTION("should handle stream interruptions")
+  {
+    using body_t = http::vector_body<std::uint8_t>;
+
+    asio::io_context io;
+
+    // fail after 300 reads
+    auto fc     = test::fail_counter(300);
+    auto stream = test::stream(io, fc);
+    auto buf    = beast::flat_buffer();
+    auto timer  = asio::steady_timer(io);
+
+    constexpr
+    auto const body_size = std::uint64_t{4096 * 1024};
+
+    {
+      auto req = http::request<body_t>(http::verb::post, "/", 11);
+      req.body() = std::vector<std::uint8_t>(body_size, 137);
+      req.prepare_payload();
+      beast::ostream(stream.buffer()) << req;
+    }
+
+    foxy::header_parser<> header_parser;
+    header_parser.body_limit(body_size);
+
+    asio::spawn(
+      io,
+      [&](asio::yield_context yield_context) -> void
+      {
+        http::async_read_header(stream, buf, header_parser, yield_context);
+
+        REQUIRE_THROWS((foxy::async_read_body<body_t>(
+          stream, buf, timer,
+          std::move(header_parser),
+          yield_context)));
+
+        io.stop();
       });
 
     io.run();
   }
-
-  // SECTION("should handle stream interruptions")
-  // {
-  //   using body_t = http::vector_body<std::uint8_t>;
-
-  //   asio::io_context io;
-
-  //   // fail after 300 reads
-  //   auto fc     = test::fail_counter(300);
-  //   auto stream = test::stream(io, fc);
-  //   auto buf    = beast::flat_buffer();
-  //   auto timer  = asio::steady_timer(io);
-
-  //   constexpr
-  //   auto const body_size = std::uint64_t{4096 * 1024};
-
-  //   {
-  //     auto req = http::request<body_t>(http::verb::post, "/", 11);
-  //     req.body() = std::vector<std::uint8_t>(body_size, 137);
-  //     req.prepare_payload();
-  //     beast::ostream(stream.buffer()) << req;
-  //   }
-
-  //   foxy::header_parser<> header_parser;
-  //   header_parser.body_limit(body_size);
-
-  //   http::read_header(stream, buf, header_parser);
-
-  //   auto fut = foxy::async_read_body<body_t>(
-  //     stream, buf, timer,
-  //     std::move(header_parser),
-  //     asio::use_future);
-
-  //   io.run();
-
-  //   REQUIRE_THROWS(fut.get());
-  // }
 }
