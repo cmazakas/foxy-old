2533aed875b1f2002db63b8534416ef9976d5a40
LeonineKing1199 <christian.mazakas@gmail.com>
2018-04-02 20:37:55 -0700

    Began prototyping the compiler abuser

--- include/foxy/route.hpp
+++ include/foxy/route.hpp
@@ -2,6 +2,10 @@
 #define FOXY_ROUTE_HPP_
 
 #include <utility>
+#include <type_traits>
+
+#include <boost/hof/if.hpp>
+#include <boost/hof/first_of.hpp>
 
 #include <boost/utility/string_view.hpp>
 
@@ -10,6 +14,9 @@
 
 #include <boost/spirit/include/qi_rule.hpp>
 #include <boost/spirit/include/qi_parse.hpp>
+
+#include <boost/callable_traits/return_type.hpp>
+#include <boost/callable_traits/has_void_return.hpp>
 
 namespace foxy
 {
@@ -48,18 +55,31 @@ auto match_route(
   boost::string_view const  sv,
   Routes             const& routes) -> bool
 {
-  namespace qi = boost::spirit::qi;
+  namespace qi  = boost::spirit::qi;
+  namespace hof = boost::hof;
 
   return boost::fusion::any(
     routes,
     [sv](auto const& route) -> bool
     {
-      auto const is_match = qi::parse(sv.begin(), sv.end(), route.rule);
-      if (is_match) {
-        // invoke handler with synthesized value
-        route.handler();
-      }
-      return is_match;
+      auto const& rule    = route.rule;
+      auto const& handler = route.handler;
+
+      using rule_type = std::decay<decltype(rule)>::type;
+      using sig_type  = typename rule_type::sig_type;
+
+      using has_void_return = boost::callable_traits::has_void_return<sig_type>;
+
+      return hof::first_of(
+        hof::if_(has_void_return())([&rule, &handler, sv](void) -> bool
+        {
+          auto const is_match = qi::parse(sv.begin(), sv.end(), rule);
+          if (is_match) {
+            handler();
+          }
+          return is_match;
+        })
+      )();
     });
 }
 } // foxy
