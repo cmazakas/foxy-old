48806acb7647d7cf8fa0993f9c85ec98e2ef4a99
LeonineKing1199 <christian.mazakas@gmail.com>
2018-04-03 17:30:06 -0700

    Added sample toolchain file

---
+++ foxy.cmake
@@ -0,0 +1,7 @@
+# Sample toolchain file for Windows users
+
+set(BOOST_ROOT "/Users/cmaza/source/boosts/boost_1_67_0_b1")
+include_directories(
+  "/vcpkg/installed/x64-windows/include/catch"
+  "/Users/cmaza/source/beast/include"
+)
