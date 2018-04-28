82dc3e65ba081e7c449f42f881346e1405b2a4b5
LeonineKing1199 <christian.mazakas@gmail.com>
2018-04-28 08:12:44 -0700

    Refactored CMake stuff

--- foxy.cmake
+++ foxy.cmake
@@ -1,9 +1,3 @@
 # Sample toolchain file for Windows users
-
-add_compile_options("/await" "/permissive-")
-set(BOOST_ROOT "/Users/cmaza/source/boosts/boost_1_67_0")
-set(Boost_USE_STATIC_LIBS ON)
-include_directories(
-  "/vcpkg/installed/x64-windows/include/catch"
-  # "/Users/cmaza/source/beast/include"
-)
+add_compile_options("/permissive-")
+include("/vcpkg/scripts/buildsystems/vcpkg.cmake")
